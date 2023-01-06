b3c9ea5f243aa41c5ed855847523bf2483eb1d50
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2023-01-07 00:11:50 +0100

    Soe launcher re work and more work

--- SOElauncher/src/main.rs
+++ SOElauncher/src/main.rs
@@ -3,42 +3,78 @@
     windows_subsystem = "windows"
 )]
 
-mod game;
+
 mod popup;
+use std::{fs, process::Command, thread};
+
 use tauri::command;
 
 #[macro_use]
 extern crate handy_macros;
 
 
-#[macro_use]
+
 extern crate lazy_static;
 
 #[command]
 fn test() {
     println!("I was invoked from JS!");
+    
 }
 
 #[command]
 fn report_backend(data: String) -> String {
     match data.as_str() {
-        "comp/game" => match game::start() {
-            true => return s!("comp/game"),
-            false => return s!("comp/game_fail"),
-        },
+       
         "comp/open_git" => return s!("comp/open_git"),
+        "comp/not_imp" => return s!("comp/not_imp"),
         a => {
             panic!("cannot find {a}")
         }
     }
 }
 
+#[command]
+fn fetch_modlist()->Vec<String>{
+    println!("l");
+    let paths = fs::read_dir("./mods").unwrap();
+    let mut mods = Vec::new();
+    for path in paths {
+        let modf = path.unwrap().path();
+        mods.push(modf.to_str().unwrap().to_string());
+        
+    }
+    mods
+}
+#[command]
+fn start (data: Vec<String>){
+    let mut s = String::new();
+    for x in data{
+        s.push_str(&format!("--mod {}  ",x));
+    }
+
+    println!("{}",s);
+    let _ = thread::spawn(|| {
+        let _ = Command::new(format!("{}", "./SymphonyOfEmpires"))
+        .arg(s)
+        .output()
+        .unwrap();
+    });
+    
+   
+}
+
+
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             test,
             popup::open_web_git,
-            report_backend
+            report_backend,
+            fetch_modlist,
+            start,
+            
         ])
         .run(tauri::generate_context!("tauri.conf.json"))
         .expect("error while running tauri application");
--- SOElauncher_old/src/popup.rs
+++
@@ -1,9 +0,0 @@
-use fltk::{app, button::Button, prelude::*, window::Window, *};
-use webbrowser;
-use SOEcommon::SOE;
-pub fn pop(text: &str){
-    let mut wind = Window::new(100, 100, 400, 300, "LUI POPUP");
-    let _ = frame::Frame::default().size_of(&wind).set_label(text);
-    wind.end();
-    wind.show();
-}
