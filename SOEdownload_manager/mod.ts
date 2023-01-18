d398e843a887da307744ff4d99fd9d475c795ae6
yrenum <federico.caprini@protonmail.com>
2023-01-18 11:03:44 +0100

    Added custom download dir option

--- SOEdownload_manager/mod.ts
+++ SOEdownload_manager/mod.ts
@@ -2,16 +2,36 @@ import { parse } from "https://deno.land/std@0.173.0/flags/mod.ts"
 import { downloadReleases } from "./updater/releases_downloader.ts"
 
 const flags = parse(Deno.args, {
-    boolean: ["update_game", "update_launcher", "restart_game", "restart_launcher", "self_update"],
-    string: ["download_mod", "game_releases_url", "launcher_releases_url", "downloader_releases_url"],
-    default:{
-        game_releases_url: "https://api.github.com/repos/symphony-of-empires/symphony-of-empires/releases",
+    boolean: [
+        "update_game",
+        "update_launcher",
+        "restart_game",
+        "restart_launcher",
+        "self_update",
+        "test",
+    ],
+    string: [
+        "download_mod",
+        "game_releases_url",
+        "launcher_releases_url",
+        "downloader_releases_url",
+        "download_dir",
+        "test_game_releases_url",
+    ],
+    default: {
+        game_releases_url:
+            "https://api.github.com/repos/symphony-of-empires/symphony-of-empires/releases",
         launcher_releases_url: "https://api.github.com/repos/symphony-of-empires/SOEutil/releases",
-        downloader_releases_url: "https://api.github.com/repos/symphony-of-empires/SOEutil/releases",
-    }
+        downloader_releases_url:
+            "https://api.github.com/repos/symphony-of-empires/SOEutil/releases",
+        download_dir:".cache/updater",
+        test_game_releases_url: "https://api.github.com/repos/yrenum/symphony-of-empires/releases",
+    },
 })
 
-if(flags.update_game) {
-    const url = new URL(flags.game_releases_url)
-    downloadReleases(url)
+if (flags.update_game) {
+    const url = !flags.test
+        ? new URL(flags.game_releases_url)
+        : new URL(flags.test_game_releases_url)
+    downloadReleases(url, flags.download_dir,"game")
 }
--- SOEdownload_manager/updater/releases_downloader.ts
+++ SOEdownload_manager/updater/releases_downloader.ts
@@ -1,21 +1,21 @@
 import { getProjectReleases } from "./releases_fetcher.ts"
 import { ensureDirSync } from "https://deno.land/std@0.173.0/fs/mod.ts"
+import { join } from "https://deno.land/std@0.172.0/path/mod.ts"
 
-export function downloadReleases(url: URL) {
+export function downloadReleases(url: URL, downloadDir: string, fileName: string) {
     getProjectReleases(url).then((data) => {
-
         const assets = data[0].assets
         assets.forEach(async (asset) => {
-
             if (asset.browser_download_url.endsWith(`${Deno.build.os}.zip`)) {
-                ensureDirSync("./cache/update")
+                ensureDirSync(downloadDir)
 
                 const fileResponse = await fetch(asset.browser_download_url)
-                const file = Deno.openSync("./cache/update/game.zip", { write: true, create: true })
-                fileResponse.body?.pipeTo(file.writable)
+                const file = Deno.openSync(`${join(downloadDir, fileName)}.zip`, {
+                    write: true,
+                    create: true,
+                })
+                await fileResponse.body?.pipeTo(file.writable)
             }
-
         })
-
     })
 }
