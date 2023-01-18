856c31338852acf4a6cf9eb269afd3fd077c2ec6
yrenum <federico.caprini@protonmail.com>
2023-01-18 11:29:21 +0100

    Added releases check

--- SOEdownload_manager/updater/releases_downloader.ts
+++ SOEdownload_manager/updater/releases_downloader.ts
@@ -4,6 +4,9 @@ import { join } from "https://deno.land/std@0.172.0/path/mod.ts"
 
 export function downloadReleases(url: URL, downloadDir: string, fileName: string) {
     getProjectReleases(url).then((data) => {
+        if(data.length == 0)
+            return
+
         const assets = data[0].assets
         assets.forEach(async (asset) => {
             if (asset.browser_download_url.endsWith(`${Deno.build.os}.zip`)) {
--- SOEdownload_manager/updater/releases_fetcher.ts
+++ SOEdownload_manager/updater/releases_fetcher.ts
@@ -1,11 +1,11 @@
 import { ReleaseData } from "./release_data.ts"
 
 export async function getProjectReleases(repo: URL): Promise<ReleaseData[]> {
-    const headers = {
+    const init = {
         headers: {
             "User-Agent":
                 "'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'",
         },
     }
-    return (await fetch(repo, headers)).json()
+    return (await fetch(repo, init)).json()
 }
